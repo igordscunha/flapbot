d7305921b9d5fdbfa85ff93a3ac283ae0ad37ecc
Igor Cunha <igordscunha@outlook.com>
2025-08-02 00:35:13 -0300

    mudando mensagem de welcome

--- events/welcome.js
+++ events/welcome.js
@@ -4,14 +4,14 @@ module.exports = {
 	name: Events.GuildMemberAdd,
 
 	execute(member) {
-		const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'bem-vindo' || channel.name === 'geral');
+		const welcomeChannel = member.guild.channels.cache.find(channel => channel.name === 'bem-vindo');
 
 		if (!welcomeChannel) return;
 
 		const welcomeEmbed = new EmbedBuilder()
 			.setColor('#00ff99')
 			.setTitle(`Seja bem vindo(a) à gangue dos marginais, ${member.displayName}!`)
-			.setDescription(`Esperamos que você se divirta em nosso servidor, ${member.user.username}.`)
+			.setDescription(`Esperamos que você se divirta em nosso servidor. Escolha a cave e seja feliz.`)
 			.setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
 			.setTimestamp();
 
