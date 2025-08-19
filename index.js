bbc07aebaad71d64722024f211d260072e362f70
Igor Cunha <igordscunha@outlook.com>
2025-08-19 11:15:50 -0300

    correção barra novo nome quando upa e nova badge

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ async function updateNicknameBadge(member, newLevel) {
         let currentName = member.nickname || member.user.username;
 
         Object.values(levelBadges).forEach(badge => {
-            currentName = currentName.replace(badge, '').trim();
+            currentName = currentName.replace(badge, '').trim().slice(0, currentName.length);
         });
 
         const newNickname = `${newBadge} | ${currentName}`;
