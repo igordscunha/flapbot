f120b95361d8b9d55ca9ff8b1f7476b9eda673e7
Igor Cunha <igordscunha@outlook.com>
2025-08-19 23:49:54 -0300

    corrigindo bug barra do nick

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ async function updateNicknameBadge(member, newLevel) {
         let currentName = member.nickname || member.user.username;
 
         Object.values(levelBadges).forEach(badge => {
-            currentName = currentName.replace(badge, '').trim().slice(0, currentName.length);
+            currentName = currentName.replace(badge, '').trim().slice(1);
         });
 
         const newNickname = `${newBadge} | ${currentName}`;
