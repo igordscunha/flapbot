4483d395f09cfed69007a987f40395514b980158
Igor Cunha <igordscunha@outlook.com>
2025-10-07 20:26:44 -0300

    3

--- utils/nicknameManager.js
+++ utils/nicknameManager.js
@@ -16,11 +16,16 @@ async function updateNicknameBadge(member, newLevel) {
     try {
         let currentName = member.nickname || member.user.globalName || member.user.username;
         const detectiveEmoji = String.fromCodePoint(0x1F575, 0xFE0F);
-        const detectiveRegex = /\u{1F575}\u{FE0F}/gu;
+        const chars = Array.from(currentName);
 
+        const filtered = chars.filter(c => c !== detectiveEmoji).join('').trim();
+
+        if(filtered !== (currentName)){
+            await member.setNickname(filtered, 'Removendo emoji detetive');
+        }
         
         // Cria uma Expressão Regular que encontra QUALQUER badge da sua lista e a remove.
-        const allBadges = Object.values(data.levelBadges).map(b => b.replace(/[.*+?^${}()|[\]\\]\u{1F575}\u{FE0F}/gu, '\\$&'));
+        const allBadges = Object.values(data.levelBadges).map(b => b.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'));
         const badgeRegex = new RegExp(allBadges.join('|'), 'gu');
         
         let cleanName = currentName.normalize('NFC').replace(badgeRegex, '').trim();
